1302bcbec617d56045539a1fb345802208ccb1ae
Aparna <hkaparna@gmail.com>
2018-07-09 08:53:37 +0530

    Changing GetStock

--- getstocknames.py
+++ getstocknames.py
@@ -1,5 +1,9 @@
 stock=["","","","",""]
-for i in range(0,4):                                  #For entering the names for four stocks 
-    stock[i]=input("Enter the names of stock")
+for i in range(0,4):                                    
+    stock[i]=raw_input("Enter the names of stock")
+
+print "\n \tSTOCK";
+print "......................."
+print "\n"
 for i in range(0,4):
-    print (stock[i])                                  #For displaying stock names
+    print "\t",stock[i]                                 
