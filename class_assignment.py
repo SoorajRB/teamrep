855c2409ee25854913f374e14224050c10966b45
Aswathy <aswathyvr15001@gmail.com>
2018-07-09 11:48:20 +0530

    Added Stock Names

--- class_assignment.py
+++ class_assignment.py
@@ -1,3 +1,11 @@
 array = ["","","",""]
 for i in range (0,4):
     array [i]= input("Enter the name for the customer: ")
+
+
+#aswathy
+    stock=["","","","",""]
+for i in range(0,4):
+    stock[i]=input("Enter the names of stock")
+for i in range(0,4):
+    print (stock[i])
