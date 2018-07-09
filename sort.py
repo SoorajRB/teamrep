02c37f0db633a36ff45d44bb3e967e40c77c22df
Aparna <hkaparna@gmail.com>
2018-07-09 07:28:21 +0530

    Pushing sort function

---
+++ sort.py
@@ -0,0 +1,16 @@
+
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [54,26,93,17]
+bubbleSort(alist)
+print(alist)
+
+
+
+#Aparna 
